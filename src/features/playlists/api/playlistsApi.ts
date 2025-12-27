1d206bd20ce54e9f9998a4031a151b1e603ecdc2
mrkrisp <krispinadin17@gmail.com>
2025-12-27 13:25:34 +0300

    Optimistic update

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -35,6 +35,36 @@ export const playlistsApi = baseApi.injectEndpoints({
         method: 'put',
         body,
       }),
+      onQueryStarted: async ({ playlistId, body }, { queryFulfilled, dispatch, getState }) => {
+        const args = playlistsApi.util.selectCachedArgsForQuery(getState(), 'fetchPlaylists')
+
+        const patchResults: any[] = []
+
+        args.forEach((arg) => {
+          patchResults.push(
+            dispatch(
+              playlistsApi.util.updateQueryData(
+                'fetchPlaylists',
+                { pageNumber: arg.pageNumber, pageSize: arg.pageSize, search: arg.search },
+                (state) => {
+                  const index = state.data.findIndex((playlist) => playlist.id === playlistId)
+                  if (index !== -1) {
+                    state.data[index].attributes = { ...state.data[index].attributes, ...body }
+                  }
+                },
+              ),
+            ),
+          )
+        })
+
+        try {
+          await queryFulfilled
+        } catch {
+          patchResults.forEach((patchResult) => {
+            patchResult.undo()
+          })
+        }
+      },
       invalidatesTags: ['Playlist'],
     }),
     uploadPlaylistCover: build.mutation<Images, { playlistId: string; file: File }>({
--- src/features/playlists/ui/EditPlaylistForm/EditPlaylistForm.tsx
+++ src/features/playlists/ui/EditPlaylistForm/EditPlaylistForm.tsx
@@ -1,10 +1,6 @@
-import type {
-  SubmitHandler,
-  UseFormHandleSubmit,
-  UseFormRegister,
-} from "react-hook-form"
-import type { UpdatePlaylistArgs } from "@/features/playlists/api/playlistsApi.types.ts"
-import { useUpdatePlaylistMutation } from "@/features/playlists/api/playlistsApi.ts"
+import type { SubmitHandler, UseFormHandleSubmit, UseFormRegister } from 'react-hook-form'
+import type { UpdatePlaylistArgs } from '@/features/playlists/api/playlistsApi.types.ts'
+import { useUpdatePlaylistMutation } from '@/features/playlists/api/playlistsApi.ts'
 
 type Props = {
   playlistId: string
@@ -14,31 +10,24 @@ type Props = {
   editPlaylistHandler: (playlist: null) => void
 }
 
-export const EditPlaylistForm = ({
-  playlistId,
-  setPlaylistId,
-  register,
-  handleSubmit,
-  editPlaylistHandler,
-}: Props) => {
+export const EditPlaylistForm = ({ playlistId, setPlaylistId, register, handleSubmit, editPlaylistHandler }: Props) => {
   const [updatePlaylist] = useUpdatePlaylistMutation()
 
-  const onSubmit: SubmitHandler<UpdatePlaylistArgs> = data => {
+  const onSubmit: SubmitHandler<UpdatePlaylistArgs> = (data) => {
     if (!playlistId) return
-    updatePlaylist({ playlistId, body: data }).then(() => {
-      setPlaylistId(null)
-    })
+    updatePlaylist({ playlistId, body: data })
+    setPlaylistId(null)
   }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <h2>Edit playlist</h2>
       <div>
-        <input {...register("title")} placeholder="title" />
+        <input {...register('title')} placeholder="title" />
       </div>
       <div>
-        <input {...register("description")} placeholder="description" />
+        <input {...register('description')} placeholder="description" />
       </div>
-      <button type={"submit"}>save</button>
+      <button type={'submit'}>save</button>
       <button onClick={() => editPlaylistHandler(null)}>cancel</button>
     </form>
   )
